a6a60176f526525d3355a8e1890818d02c4b8e50
Sachin Kumar Behera <sachin.behera@attryb.com>
2024-02-01 20:45:48 +0530

    forth

---
+++ main.js
@@ -0,0 +1,10 @@
+function validate(form) {
+
+    if(!valid) {
+        alert('Please correct the errors in the form!');
+        return false;
+    }
+    else {
+        return confirm('Do you really want to submit the form?');
+    }
+}
